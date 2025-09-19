c7d3bf6973eec2bd3bc6f62444e00734b25853b3
tlm <tlm@EU3146027013644.local>
2025-09-18 21:02:46 -0300

    test_demo_commit

---
+++ Day1/test_demo.py
@@ -0,0 +1,8 @@
+# test_demo.py
+from selenium import webdriver
+
+def test_verify_demoqa_title () :
+    driver = webdriver.Chrome()
+    driver.get("https://demoqa.com")
+    assert "DEMOQA" in driver.title
+    driver.quit()
