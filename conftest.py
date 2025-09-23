6793cfe438a43390b3552174d3a25827a6581151
tlm <tlm@EU3146027013644.local>
2025-09-23 17:45:22 -0300

    Atualização LOG_FILE conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
 import json
+import time
+from pathlib import Path
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome", help="browser to execute tests (chrome or firefox)")
@@ -24,3 +26,25 @@ def driver(request):
 def test_data():
     with open("data/test_data.json") as f:
         return json.load(f)
+    
+    
+LOG_FILE = Path("test_durations.log")
+@pytest.hookimpl(tryfirst=True)
+def pytest_runtest_setup(item):
+    item.start_time = time.time()
+    item.start_str = time.strftime("%H:%M:%S", time.localtime())
+    msg = f"\n[START] Test '{item.nodeid}' - {item.start_str}"
+    print(msg)
+    
+    with LOG_FILE.open("a", encoding="utf-8") as f:
+        f.write(msg + "\n")
+
+@pytest.hookimpl(trylast=True)
+def pytest_runtest_teardown(item):
+    duration = time.time() - item.start_time
+    msg = f"[END] Test '{item.nodeid}' finished in {duration:.2f} seconds."
+    print(msg)
+
+    # salva em arquivo
+    with LOG_FILE.open("a", encoding="utf-8") as f:
+        f.write(msg + "\n")
