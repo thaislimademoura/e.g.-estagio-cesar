8d191dd5cde8107a38be0ad6031c131e503656cc
tlm <tlm@EU3146027013644.local>
2025-09-24 00:38:12 -0300

    test_modal_dialogs.py

--- Day4/test_modal_dialogs.py
+++ Day4/test_modal_dialogs.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
-#from selenium.webdriver.common.action_chains import ActionChains
 import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 def test_modal_dialogs(driver):
     driver.get("https://demoqa.com/modal-dialogs")
@@ -10,8 +11,6 @@ def test_modal_dialogs(driver):
     small_modal_click_message = driver.find_element(By.CSS_SELECTOR, ".modal-body")
     assert "This is a small modal. It has very less content" in small_modal_click_message.text
 
-    time.sleep(3)
-
     small_close_btn = driver.find_element(By.ID, "closeSmallModal")
     small_close_btn.click()
 
@@ -19,8 +18,13 @@ def test_modal_dialogs(driver):
     large_modal_click_btn.click()
     large_modal_click_message = driver.find_element(By.CSS_SELECTOR, ".modal-body")
     assert "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." in large_modal_click_message.text
-
-    time.sleep(3)
+    
+    wait = WebDriverWait(driver, 10)
+    
+    large_modal_click_message = wait.until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, ".modal-body"))
+    )
+    large_modal_click_message.is_displayed()
     
     large_close_btn = driver.find_element(By.ID, "closeLargeModal")
     large_close_btn.click()
