6d158f9ce59103987a9193c858fa6adc6324fb38
tlm <tlm@EU3146027013644.local>
2025-10-05 15:49:38 -0300

    12 steps

--- mobile/test_mini_challenge_1.py
+++ mobile/test_mini_challenge_1.py
@@ -5,6 +5,7 @@ from appium.webdriver.common.appiumby import AppiumBy
 
 expected_title = "Sauce Labs Backpack (orange)"
 expected_my_cart_title = "My Cart"
+expected_product_name = "Sauce Labs Backpack (orange)"
 
 options = AppiumOptions()
 options.load_capabilities({
@@ -61,7 +62,7 @@ time.sleep(2)
 
 qnt_box_after_plus = int(driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/noTV").text)
 
-assert qnt_box_after_plus == qnt_box_before_plus +1
+assert qnt_box_after_plus == qnt_box_before_plus +1, f"Expected title to be '{qnt_box_before_plus + 1}', but found '{qnt_box_after_plus}'"
 
 #add_to_cart_btn = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cartBt")
 assert add_to_cart_btn.is_enabled()
@@ -69,7 +70,7 @@ assert add_to_cart_btn.is_enabled()
 # add another unit
 plus_item_btn.click()
 qnt_item_box_actual = int(driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/noTV").text)
-assert qnt_item_box_actual == 2
+assert qnt_item_box_actual == qnt_box_after_plus +1, f"Expected title to be '{qnt_box_after_plus + 1}', but found '{qnt_box_before_plus}'"
 add_to_cart_btn.click()
 number_cart_circle = int(driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cartTV").text)
 assert number_cart_circle == qnt_item_box_actual
@@ -79,8 +80,24 @@ cart_btn = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/
 cart_btn.click()
 
 actual_my_cart_title = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().resourceId("com.saucelabs.mydemoapp.android:id/productTV")').text
+assert actual_my_cart_title == expected_my_cart_title, f"Expected title to be '{expected_my_cart_title}', but found '{actual_my_cart_title}'"
 time.sleep(2)
-assert actual_title == expected_my_cart_title, f"Expected title to be '{expected_my_cart_title}', but found '{actual_my_cart_title}'"
+
+actual_product_name = driver.find_element(AppiumBy. ID, "com.saucelabs.mydemoapp.android:id/titleTV").text
+assert actual_product_name == expected_product_name, f"Expected title to be '{expected_product_name}', but found '{actual_product_name}'"
+
+# validate value
+#final_unit_price = int(driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/priceTV").text)
+# valor Unit 
+# qnt item 
+# total 
+# converter valores float 
+# total esperado = unit * qnt item 
+# assert total = total esperado 
+
+# validate quantity
+qnt_final_cart = int(driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/noTV").text)
+assert qnt_final_cart == qnt_item_box_actual
 
 time.sleep(2)
 
