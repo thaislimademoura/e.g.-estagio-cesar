5357bebcbfc788dfa3f20d25321fb0e1a1ca5c78
tlm <tlm@EU3146027013644.local>
2025-10-06 21:05:11 -0300

    test_mini_challenge_1 complete

--- mobile/test_mini_challenge_1.py
+++ mobile/test_mini_challenge_1.py
@@ -1,4 +1,5 @@
 import time
+
 from appium import webdriver
 from appium.options.common.base import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
@@ -12,16 +13,17 @@ unit_price = 29.99
 username_without_password = "thais"
 full_name = "algum nome"
 address_line_1 = "abcd"
-address_line_2 = "abcd"
+address_line_2 = "abcdefg"
 city = "Recife"
 state = "PE"
 zip_code = "11111111"
 country = "Brasil"
 card_number = "1111111111111111"
-expiration_date = "1111"
+expiration_date = "11/11"
 security_code = "111"
+cart_items = 0
 
-
+# 1
 options = AppiumOptions()
 options.load_capabilities({
 	"platformName": "Android",
@@ -41,16 +43,17 @@ options.load_capabilities({
 driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
 time.sleep(4)
 
+# 2
 orange_backpack = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().resourceId("com.saucelabs.mydemoapp.android:id/productIV").instance(2)')
 orange_backpack.click()
 time.sleep(2)
 
-# assert backpack orange opened
+# 3 - assert backpack orange opened
 
 actual_title = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/productTV").text
 assert actual_title == expected_title, f"Expected title to be '{expected_title}', but found '{actual_title}'"
 
-# assert minus
+# 4 - assert minus
 
 qnt_box_before = int(driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/noTV").text)
 
@@ -114,7 +117,7 @@ assert qnt_final_cart == qnt_final_product
 total_item = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/itemsTV").text
 total_item_final = int(total_item.split()[0])
 
-assert total_item_final == 2
+assert total_item_final == qnt_final_product
 
 # validate total price 
 total_price = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/totalPriceTV").text
@@ -211,16 +214,78 @@ review_screen = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelecto
 assert review_screen.is_displayed()
 time.sleep(2)
 
-# my_cart_unit_price = float(driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/priceTV").text)
-# assert my_cart_unit_price == product_unit_price, f"Expected value to be '{product_unit_price}', but found '{my_cart_unit_price}'"
-# valor Unit 
-# qnt item 
-# total 
-# converter valores float 
-# total esperado = unit * qnt item 
-# assert total = total esperado 
+# validate address and payment 
+output_full_name = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/fullNameTV")
+assert full_name in output_full_name.text
+
+output_address_line_1 = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/addressTV")
+assert address_line_1 in output_address_line_1.text
+
+output_city_state = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cityTV")
+assert city in output_city_state.text
+assert state in output_city_state.text
+
+output_zip_code_country = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/countryTV")
+assert zip_code in output_zip_code_country.text
+assert country in output_zip_code_country.text
+
+output_card_name = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cardHolderTV")
+assert full_name in output_card_name.text
+
+output_card_number = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cardNumberTV")
+assert card_number in output_card_number.text
 
 
+exp_id = "com.saucelabs.mydemoapp.android:id/expirationDateTV"
+scrolling = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, f'new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().resourceId("{exp_id}"));')
+
+
+output_expiration_date = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/expirationDateTV")
+assert expiration_date in output_expiration_date.text
+time.sleep(2)
+
+product_title_id = "com.saucelabs.mydemoapp.android:id/titleTV"
+scrolling = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, f'new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().resourceId("{product_title_id}"));')
+
+# 34 - validate products unit information
+product_title = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/titleTV").text
+assert product_title == expected_title, f"Expected title to be '{expected_title}', but found '{actual_title}'"
+
+product_price = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/priceTV").text
+assert product_price == product_unit_price, f"Expected price to be '{product_unit_price}', but found '{product_price}'"
+
+# 35 - validate total value
+final_value = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/totalAmountTV").text
+final_value_float = float(final_value.replace("$","").strip())
+
+delivery_id = "com.saucelabs.mydemoapp.android:id/amountTV"
+scrolling = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, f'new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().resourceId("{delivery_id}"));')
+
+delivery = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/amountTV").text
+delivery_float = float(delivery.replace("$","").strip())
+assert final_value_float == delivery_float + (unit_price * 2)
+
+# 36 - place order button
+place_order_btn = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/paymentBtn").click()
+time.sleep(2)
+
+# 37 - validate checkout complete screen
+checkout_complete_screen = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.ViewGroup").instance(2)')
+assert checkout_complete_screen.is_displayed
+
+# 38 - continue shopping button
+continue_shopping_btn = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/shoopingBt").click()
+time.sleep(2)
+products_screen = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.ViewGroup").instance(2)')
+assert products_screen.is_displayed()
+
+cart_empty = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cartIV")
+cart_full = driver.find_elements(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().resourceId("com.saucelabs.mydemoapp.android:id/cartTV")')
+
+#assert cart_empty.is_displayed() and not cart_full.is_displayed()
+assert cart_empty.is_displayed() and not cart_full
+
+driver.quit()
 
 
 
