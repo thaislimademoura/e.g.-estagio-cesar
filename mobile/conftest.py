7d9be28d87e308a222efb674d11a4615a6de5bf6
tlm <tlm@EU3146027013644.local>
2025-10-12 19:35:32 -0300

    conftest.py - mobile

--- mobile/conftest.py
+++ mobile/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from appium import webdriver
 from appium.options.common.base import AppiumOptions
+import json
+from pathlib import Path
 
 @pytest.fixture(scope="function")
 def driver():
@@ -22,3 +24,23 @@ def driver():
     # This code runs AFTER the test function completes (or fails)
     print("\nQuitting driver...")
     _driver.quit()
+
+
+@pytest.fixture(scope="session")
+def load_data_capabilities():
+    """Lê o JSON de capabilities e retorna como dicionário"""
+    json_path = Path(__file__).parent / "data" / "data_test_mydemoapp_1.json"
+    with open(json_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return data
+
+
+def test_android_capabilities(mobile_capabilities):
+    android_caps = mobile_capabilities["android"]
+    assert android_caps["platformName"] == "Android"
+    assert "deviceName" in android_caps
+
+
+def test_ios_capabilities(mobile_capabilities):
+    ios_caps = mobile_capabilities["ios"]
+    assert ios_caps["automationName"] == "XCUITest"
